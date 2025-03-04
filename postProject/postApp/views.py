ca3f276040ab0c6ede69025cdf2a5ab93a3f5540
agyanim <agyanimconsultancy@gmail.com>
2025-03-04 15:33:43 +0000

    create user form added

--- postProject/postApp/views.py
+++ postProject/postApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .form import UserForm
 
 # Create your views here.
 
@@ -8,3 +9,12 @@ def index(request):
 
 def login(request):
     return render(request, 'postApp/login.html')
+
+def createUser(request):
+    form = UserForm()
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context= {'form': form}
+    return render(request, 'postApp/create-user.html', context)
--- postProject/postProject/settings.py
+++ postProject/postProject/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'postApp',
-     'tailwind',
-     'theme',
-     'django_browser_reload',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
+    'widget_tweaks',
      
      
 ]
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
