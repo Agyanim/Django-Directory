848fe83e50bdecaf79c26b4cb20331cce0c11412
agyanim <agyanimconsultancy@gmail.com>
2025-03-11 17:07:24 +0000

    Post model added in models

--- postProject/postApp/admin.py
+++ postProject/postApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Posts
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Posts)
--- postProject/postApp/models.py
+++ postProject/postApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=255, choices = [('FICTION' ,'Fiction'), ('BIOGRAPHY','Biography' ), ('ROMANS','Romans'), ('POETRY','Poetry'),], default='FICTION')
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta(): 
+       ordering = ['-createdAt' ]
+    
